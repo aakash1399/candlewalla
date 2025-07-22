48221aa3a37703908f924170ecceee9d9eef5a5f
aakash1399 <aakashpatil1997@gmail.com>
2025-07-22 17:37:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('stock-selector')?.addEventListener('change', function() {
-  const selectedStock = this.value;
-  document.getElementById('live-chart').innerHTML = `<iframe src='https://s.tradingview.com/widgetembed/?symbol=${selectedStock}&interval=5&theme=light' width='100%' height='400'></iframe>`;
-});
